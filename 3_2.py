063cb329873714e19b74e97e89a4e91f8724bcb5
Stanislaw Szwiec <szwiec.st@jacobs.com>
2022-03-23 12:33:50 +0100

    Split shopping list into shops

--- 3_2.py
+++ 3_2.py
@@ -2,5 +2,9 @@ zakupy = {
     "Piekarnia": ["Chleb", "Paczek", "Bulki"],
     "Warzywniak": ["Marchew", "Seler", "Rukola"]
 }
-
+print("Lista zakupow")
+a=0
+for i in zakupy:
+    print(f"Ide do {i}, kupuje tu nastepujace rzeczy: {zakupy[i]}")
+    a=a+len(zakupy[i])
 
